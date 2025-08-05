0b33489de4781f1107755c6643639528aed4dc79
munkf <iimanfirdaus@gmail.com>
2025-08-05 14:23:10 +0800

    made changes to Work.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -75,7 +75,7 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='w-full flex flex-col'>
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
